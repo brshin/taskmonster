38e2fe0f847e8477cef494df0a600f30611c5a64
evanhekman <ehekman@umich.edu>
2023-11-19 05:18:46 -0500

    fully streamlined shop

--- mhacks_app/lib/shop.dart
+++ mhacks_app/lib/shop.dart
@@ -55,74 +55,6 @@ class Shop {
     'Excalibur': ItemStat(25, 0, 0, false, 200, 30),
   };
 
-  static const Map<String, int> masterShopMap = {
-    'Sword': 1,
-    'Apple': 2,
-    'Shield': 3,
-    'Health Potion': 4,
-    'Magic Wand': 5,
-    'Helmet': 6,
-    'Gold Sword': 7,
-    'Bow': 8,
-    'Mana Elixir': 9,
-    'Diamond Sword': 10,
-    'Platinum Shield': 11,
-    'Enchanted Robe': 12,
-    'Dagger': 13,
-    'Crystal Ball': 14,
-    'Steel Armor': 15,
-    'Fire Staff': 16,
-    'Elixir of Wisdom': 17,
-    'Invisibility Cloak': 18,
-    'Thunder Hammer': 19,
-    'Phoenix Feather': 20,
-    'Ice Blade': 21,
-    'Dragon Scale Armor': 22,
-    'Vortex Orb': 23,
-    'Shadow Cloak': 24,
-    'Mystic Bow': 25,
-    'Amulet of Power': 26,
-    'Celestial Staff': 27,
-    'Titanium Shield': 28,
-    'Dimensional Robes': 29,
-    'Excalibur': 30,
-  };
-
-  // contains the prices of all items, with the name as the key
-  // KEY: Item name, VALUE: Item cost
-  static const Map<String, int> costMap = {
-    'Sword': 10,
-    'Apple': 20,
-    'Shield': 5,
-    'Health Potion': 15,
-    'Magic Wand': 30,
-    'Helmet': 25,
-    'Gold Sword': 40,
-    'Bow': 35,
-    'Mana Elixir': 20,
-    'Diamond Sword': 60,
-    'Platinum Shield': 50,
-    'Enchanted Robe': 45,
-    'Dagger': 35,
-    'Crystal Ball': 40,
-    'Steel Armor': 45,
-    'Fire Staff': 50,
-    'Elixir of Wisdom': 55,
-    'Invisibility Cloak': 60,
-    'Thunder Hammer': 65,
-    'Phoenix Feather': 70,
-    'Ice Blade': 75,
-    'Dragon Scale Armor': 80,
-    'Vortex Orb': 85,
-    'Shadow Cloak': 90,
-    'Mystic Bow': 95,
-    'Amulet of Power': 100,
-    'Celestial Staff': 105,
-    'Titanium Shield': 110,
-    'Dimensional Robes': 115,
-    'Excalibur': 200,
-  };
-
   // temporary shop map of items currently available
   // KEY: Item name, VALUE: Unlock level
   // INVARIANT: subset of masterShopMap
@@ -141,8 +73,6 @@ class Shop {
     return showDialog(
         context: context,
         builder: (_) => AlertDialog(
-              // shape: RoundedRectangleBorder(
-              //   borderRadius: BorderRadius.all(Radius.circular(10.0))),
               content: Builder(
                 builder: (context) {
                   var height = MediaQuery.of(context).size.height;
@@ -166,27 +96,9 @@ class Shop {
                         }
                       },
                     ),
-                    /*
-                    child: Column(
-                      //mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        for (var item in currentShopMap.entries)
-                          if (itemAvailable(item.key, player))
-                            ShopItem(
-                              itemName: item.key,
-                              itemPrice: costMap[item.key] ?? -1,
-                              onPressed: () => purchaseItem(item.key, player),
-                            ),
-                      ],
-                    ),
-                    */
                   );
                 },
               ),
-              // title: Text(''),
-              // actions: [
-
-              // ],
             ));
   }
 
@@ -244,115 +156,3 @@ class ShopItem extends StatelessWidget {
     );
   }
 }
-
-
-// class _ShopPageState extends State<ShopPage> {
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Shop'),
-//       ),
-//       body: Padding(
-//         padding: const EdgeInsets.all(16.0),
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [ 
-//             for (var entry in storeInventory.entries)
-//               if (canPlayerPurchaseItem(entry.key))
-//                 ShopItem(
-//                   itemName: entry.key,
-//                   itemPrice: getItemPrice(entry.key),
-//                   onPressed: () => buyItem(entry.key),
-//                 ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-
-
-
-//   void buyItem(String itemName) {
-//     if (storeInventory.containsKey(itemName) && storeInventory[itemName]! > 0) {
-//       if (canPlayerPurchaseItem(itemName)) {
-//         int itemPrice = getItemPrice(itemName);
-
-//         if (widget.player.currency.canAfford(itemPrice)) {
-//           widget.player.currency.subtractGold(itemPrice);
-//           givePurchasedItem(itemName);
-//           storeInventory[itemName] = storeInventory[itemName]! - 1;
-//         } else {
-//           print('Not enough gold to buy the $itemName.');
-//         }
-//       } else {
-//         print('Item $itemName is not available at your level.');
-//       }
-//     } else {
-//       print('Sorry, $itemName is out of stock.');
-//     }
-//   }
-
-//   void givePurchasedItem(String itemName) {
-//     print('You obtained a $itemName!');
-//   }
-// }
-
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return ElevatedButton(
-//       onPressed: onPressed,
-//       child: Padding(
-//         padding: const EdgeInsets.all(8.0),
-//         child: Row(
-//           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//           children: [
-//             Text('$itemName'),
-//             Text('$itemPrice gold'),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-
-/* NO LONGER NEEDED BUT NOT QUITE SURE */
-
-  // int getItemPrice(String itemName) {
-  //   return {
-  //     'Sword': 10,
-  //     'Apple': 5,
-  //     'Shield': 15,
-  //     'Health Potion': 8,
-  //     'Magic Wand': 30,
-  //     'Helmet': 25,
-  //     'Gold Sword': 40,
-  //     'Bow': 35,
-  //     'Mana Elixir': 20,
-  //     'Diamond Sword': 60,
-  //     'Platinum Shield': 50,
-  //     'Enchanted Robe': 45,
-  //     'Dagger': 35,
-  //     'Crystal Ball': 40,
-  //     'Steel Armor': 45,
-  //     'Fire Staff': 50,
-  //     'Elixir of Wisdom': 55,
-  //     'Invisibility Cloak': 60,
-  //     'Thunder Hammer': 65,
-  //     'Phoenix Feather': 70,
-  //     'Ice Blade': 75,
-  //     'Dragon Scale Armor': 80,
-  //     'Vortex Orb': 85,
-  //     'Shadow Cloak': 90,
-  //     'Mystic Bow': 95,
-  //     'Amulet of Power': 100,
-  //     'Celestial Staff': 105,
-  //     'Titanium Shield': 110,
-  //     'Dimensional Robes': 115,
-  //     'Excalibur': 120,
-  //   }[itemName] ??
-  //       0;
-  // }
